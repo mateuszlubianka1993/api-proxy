99b1ddd9b5c20f757f80d48c1065841d2185a3fd
mateuszlubianka1993 <lubiankamateusz@gmail.com>
2025-09-30 23:42:04 +0200

    Add cors whitelist logic

--- src/app.js
+++ src/app.js
@@ -1,10 +1,25 @@
 import express from "express";
 import cors from "cors";
 import newsRoutes from "./routes/newsRoutes.js";
+import whitelist from "./config/cors-whitelist.js";
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) {
+      return callback(null, true);
+    }
+
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error(`CORS: Access denied for ${origin}`), false);
+    }
+  }
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/news", newsRoutes);
---
+++ src/config/cors-whitelist.js
@@ -0,0 +1,6 @@
+const whitelist = [
+  "http://localhost:3000",
+  "https://mateuszlubianka1993.github.io"
+];
+
+export default whitelist;
