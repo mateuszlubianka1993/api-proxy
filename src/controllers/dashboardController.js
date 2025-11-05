e78b8d45d4af7e05889e172cef1adf7bd4aecef3
mateuszlubianka1993 <lubiankamateusz@gmail.com>
2025-11-05 14:58:39 +0100

    Add simple dashboard view with fake data

--- src/controllers/dashboardController.js
+++ src/controllers/dashboardController.js
@@ -1,6 +1,15 @@
+import fs from "fs";
+import path from "path";
+
+const dataPath = path.join(process.cwd(), "src", "data", "apiConfigs.json");
+
 export const showDashboard = (req, res) => {
+  const data = fs.readFileSync(dataPath, "utf-8");
+  const apiConfigs = JSON.parse(data);
+
   res.render('dashboard', {
     title: 'Dashboard',
-    user: req.session.user
+    user: req.session.user,
+    apiConfigs
    });
 };
